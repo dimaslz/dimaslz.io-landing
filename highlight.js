2cd14cc87dbd42656fefff831bd73eed1e9698d7
dimaslz <me@dimaslz.io>
2020-02-24 14:06:07 +0100

    feat(*): Initial commit.

---
+++ highlight.js
@@ -0,0 +1,31 @@
+export default function (json) {
+  json = json
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+
+  json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+
+    var cls = 'number';
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = 'key';
+      } else {
+        cls = 'string';
+      }
+    } else if (/true|false/.test(match)) {
+      cls = 'boolean';
+    } else if (/null/.test(match)) {
+      cls = 'null';
+    }
+    return '<span class="' + cls + '">' + match + '</span>';
+  });
+
+  json = json
+    .replace('[', '<span class="open-array">[</span>')
+    .replace(']', '<span class="close-array">]</span>')
+    .replace('{', '<span class="open-object">{</span>')
+    .replace('}', '<span class="close-object">}</span>');
+
+  return `<span class="syntax-wrapper">${json}</span>`;
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+
+import main from './main';
+
+const json = `{
+  name: "Dimas López Zurita",
+  email: "me@dimaslz.io",
+  keywords: [
+    "javascript",
+    "vue",
+    "react",
+    "angular",
+    "nodejs+express, nest",
+    "software",
+    "android",
+    "ios",
+  ],
+  network: {
+    linkedin: "https://www.linkedin.com/in/dimaslopezzurita",
+    github: "https://github.com/dimaslz",
+    twitter: "https://twitter.com/dimaslz"
+  },
+  former_jobs: ["Trovit", "BeRepublic", "FocusOnEmotions", "Zyncro", "Instaply", "Ubeeqo/Europcar Mobility Group"],
+  localtion: "Barcelona, Spain",
+  status: "Working in myself",
+  interests: "Entrepreneurship, business, webapp, javascript, android, ios, ..."
+}`;
+
+CodeMirror(document.body, {
+  value: json,
+  mode:  "javascript",
+  theme: "material",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+main();
